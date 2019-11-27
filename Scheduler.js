d66ac797dd8552dcbce4c301c6e4a7551bc7680e
Salvatore Manna <salvm93@gmail.com>
2019-11-27 09:56:19 +0100

    Scheduler

--- Scheduler.js
+++ Scheduler.js
@@ -1,233 +1,307 @@
 import React, { Component } from "react";
 import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  Dimensions,
-  View,
-  TouchableOpacity,
-  Modal
+    ScrollView,
+    StyleSheet,
+    Text,
+    Dimensions,
+    View,
+    TouchableOpacity,
+    TouchableHighlight,
+    Modal
 } from "react-native";
 
 const WIDTH = Dimensions.get("window").width;
 const WEEK = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 export default class Scheduler extends Component {
-  static navigationOptions = {
-    title: "Awesome Scheduler"
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      Mon: [],
-      Tue: [
-        { id: 102, title: "Table Tennis" },
-        { id: 104, title: "Swimming" }
-      ],
-      Wed: [{ id: 103, title: "Cycling" }],
-      Thu: [{ id: 108, title: "Rest" }],
-      Fri: [
-        { id: 103, title: "Badminton" },
-        { id: 109, title: "KickBoxing" }
-      ],
-      selectedActivity: null,
-      daySelected: null,
-      displayModal: false
+    static navigationOptions = {
+        title: "Awesome Scheduler"
     };
-  }
 
-  componentWillMount() {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            activities: {
+                Mon: [],
+                Tue: [
+                    { id: 102, title: "Table Tennis" },
+                    { id: 104, title: "Swimming" }
+                ],
+                Wed: [{ id: 103, title: "Cycling" }],
+                Thu: [{ id: 108, title: "Rest" }],
+                Fri: [
+                    { id: 103, title: "Badminton" },
+                    { id: 109, title: "KickBoxing" }
+                ]
+            },
+            selectedActivity: null,
+            selectedActivityDay: null,
+            defaultSelectedDay: null,
+            displayModal: false
+        };
+    }
 
-  onLongPressed = (id, day) => {
-    this.selectedActivityDay = day;
-    this.setState({ selectedActivity: id, displayModal: true });
-  };
+    componentWillMount() {}
 
-  onModalClose = () => {
-    this.setState({
-      displayModal: false,
-      selectedActivity: null,
-      daySelected: null
-    });
-  };
+    onLongPressed = (id, day) => {
+        this.setState({ selectedActivityDay: day, defaultSelectedDay: day, selectedActivity: id, displayModal: true });
+    };
 
-  onMoveActivity = () => {};
+    get initialModalValues() {
+        return {
+            displayModal: false,
+            selectedActivity: null,
+            selectedActivityDay: null,
+            defaultSelectedDay: null
+        }
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.headerView}>
-          <Text style={styles.bannerText}> Toruk Macto Schedule week 1 </Text>
-        </View>
-        <View style={styles.content}>
-          {WEEK.map(day => {
+    onModalClose = () => {
+        this.setState(this.initialModalValues);
+    };
+
+    onChangeSelectedDay = (newDay) => {
+        this.setState({selectedActivityDay: newDay});
+    };
+
+    onMoveActivity = () => {
+        const {activities, defaultSelectedDay, selectedActivityDay, selectedActivity} = Object.assign({}, this.state);
+        let prevDayActivities = Object.assign([], activities[defaultSelectedDay]);
+        let nextDayActivities = Object.assign([], activities[selectedActivityDay]);
+        let activity = (prevDayActivities.find(e => e.id == selectedActivity));
+        activity = JSON.parse(JSON.stringify(activity));
+        let activityIndex = prevDayActivities.indexOf(activity);
+        prevDayActivities.splice(activityIndex, 1);
+        nextDayActivities.push(activity);
+        //activities[defaultSelectedDay] = prevDayActivities;
+        //activities[selectedActivityDay] = nextDayActivities;
+        // activities[defaultSelectedDay.toString()] = prevDayActivities.filter(e => e.id !== activityIndex);
+        // activities[selectedActivityDay.toString()] = nextDayActivities.concat(activity);
+        // const newState = {...this.initialModalValues, activities: updatedActivities};
+        console.warn(prevDayActivities);
+        console.warn(nextDayActivities);
+        this.setState({
+            ...this.initialModalValues,
+            activities : {
+                ...activities,
+                [defaultSelectedDay]: prevDayActivities,
+                [selectedActivityDay]: nextDayActivities
+            }
+        });
+    };
+
+    render() {
+        const renderDayButton = (day, isSelected) => {
             return (
-              <View key={day} style={styles.dayView}>
-                <View style={styles.dayTextView}>
-                  <Text>{day}</Text>
+                <TouchableHighlight key={day} style={[styles.modalDaysButton, isSelected ? styles.modalDaysButtonSelected : null]}
+            onPress={() => this.onChangeSelectedDay(day)}>
+        <Text style={{ textAlign: "center" }}>{day}</Text>
+            </TouchableHighlight>
+        );
+        };
+        const renderDayButtonsContainer = days => {
+            const {selectedActivityDay} = this.state;
+            if(selectedActivityDay) {
+                return days.map(day => {
+                    const isSelected = selectedActivityDay === day;
+                    return (renderDayButton(day, isSelected));
+                });
+            } else {
+                return null;
+            }
+        };
+        const renderActivities = () => {
+            return WEEK.map(day => {
+                return (
+                    <View key={day} style={styles.dayView}>
+            <View style={styles.dayTextView}>
+            <Text>{day}</Text>
                 </View>
                 <DaySchedule
-                  day={day}
-                  data={this.state[day]}
-                  onLongPressed={this.onLongPressed}
+                day={day}
+                data={this.state.activities[day]}
+                onLongPressed={this.onLongPressed}
                 />
-              </View>
+                </View>
             );
-          })}
+            });
+        };
+        return (
+            <View style={styles.container}>
+    <View style={styles.headerView}>
+    <Text style={styles.bannerText}> Toruk Macto Schedule week 1 </Text>
         </View>
+        <View style={styles.content}>
+        {renderActivities()}
+    </View>
         <Modal
-          animationType="slide"
-          visible={this.state.displayModal}
-          onRequestClose={this.onModalClose}
-          transparent
+        animationType="slide"
+        visible={this.state.displayModal}
+        onRequestClose={this.onModalClose}
+        transparent
         >
-          <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-              <View>
-                <Text style={{ textAlign: "center" }}>
-                  Select a date to move the activity
-                </Text>
-              </View>
+        <View style={styles.modalContainer}>
+    <View style={styles.modalContent}>
+    <View>
+        <Text style={{ textAlign: "center" }}>
+        Select a date to move the activity
+        </Text>
+        </View>
 
-              <View style={styles.modalButtonContainer}>
-                <TouchableOpacity
-                  style={styles.buttonView}
-                  onPress={this.onModalClose}
-                >
-                  <Text style={styles.buttonText}> Cancel </Text>
-                </TouchableOpacity>
+        <View style={styles.modalDaysContainer}>
+        {renderDayButtonsContainer(WEEK)}
+    </View>
 
-                <TouchableOpacity
-                  disabled={
-                    !this.state.daySelected || !this.selectedActivityDay
-                  }
-                  style={[
-                    styles.buttonView,
-                    {
-                      opacity:
-                        !this.state.daySelected || !this.selectedActivityDay
-                          ? 0.2
-                          : 1
-                    }
-                  ]}
-                  onPress={this.onMoveActivity}
-                >
-                  <Text style={styles.buttonText}> Move </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
+        <View style={styles.modalButtonContainer}>
+    <TouchableOpacity
+        style={styles.buttonView}
+        onPress={this.onModalClose}
+    >
+    <Text style={styles.buttonText}> Cancel </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        disabled={this.state.selectedActivityDay === this.state.defaultSelectedDay}
+        style={[
+                styles.buttonView,
+        {
+            opacity:
+                this.state.selectedActivityDay === this.state.defaultSelectedDay
+                    ? 0.2
+                    : 1
+        }
+    ]}
+        onPress={this.onMoveActivity}
+    >
+    <Text style={styles.buttonText}> Move </Text>
+        </TouchableOpacity>
+        </View>
+        </View>
+        </View>
         </Modal>
-      </View>
+        </View>
     );
-  }
+    }
 }
 
 class DaySchedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        data : this.props.data
-    };
-  }
-
-  componentDidMount = () => {};
-
-  componentWillReceiveProps = () => {};
-
-  render() {
-    const { data } = this.props;
-    if (!data) {
-      return;
+    constructor(props) {
+        super(props);
     }
 
-    return (
-      <View style={styles.dayScheduleScrollView}>
-        {this.state.data.map(({ title, id }) => (
-          <TouchableOpacity
+    componentDidMount = () => {};
+
+    componentWillReceiveProps = () => {};
+
+    render() {
+        const { data } = this.props;
+
+        return (
+            <View style={styles.dayScheduleScrollView}>
+        {data.map(({ title, id }) => (
+            <TouchableOpacity
             style={styles.activityItemView}
             key={id}
             delayLongPress={800}
             onLongPress={() => {
-              this.props.onLongPressed(id, this.props.day);
-            }}
-          >
-            <Text style={styles.activityText}>{title}</Text>
-          </TouchableOpacity>
+            this.props.onLongPressed(id, this.props.day);
+        }}
+        >
+        <Text style={styles.activityText}>{title}</Text>
+        </TouchableOpacity>
         ))}
-      </View>
+    </View>
     );
-  }
+    }
 }
 
 //////////// STYLES ////////////////////////
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  headerView: { flex: 0.1, borderBottomWidth: 1 },
-  contentView: { flex: 0.9 },
-  bannerText: {
-    fontWeight: "700",
-    fontSize: 16,
-    textAlign: "center",
-    padding: 10
-  },
-  activityText: { color: "white" },
-  activityItemView: {
-    backgroundColor: "grey",
-    borderRadius: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    justifyContent: "center",
-    marginHorizontal: 8
-  },
-  dayView: {
-    flexDirection: "row",
-    borderBottomWidth: 1,
-    justifyContent: "space-between",
-    height: 50,
-    alignItems: "center"
-  },
-  dayTextView: { alignItems: "center", justifyContent: "center" },
-  dayScheduleScrollView: { flex: 1, marginVertical: 10, flexDirection: "row" },
-  buttonText: { textAlign: "center" },
-  buttonView: {
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-    borderRadius: 5,
-    borderWidth: 1
-  },
-  modalContainer: {
-    backgroundColor: "rgba(0,0,0,0.3)",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1
-  },
-  modalContent: {
-    backgroundColor: "white",
-    justifyContent: "space-around",
-    alignItems: "center",
-    height: 250,
-    position: "absolute",
-    bottom: 0
-  },
-  modalButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: WIDTH,
-    marginVertical: 15
-  },
-  dayButton: {
-    padding: 10,
-    justifyContent: "center",
-    borderRadius: 5,
-    alignItems: "center"
-  },
-  daySelectorView: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: WIDTH,
-    paddingHorizontal: 10
-  }
+    container: { flex: 1 },
+    headerView: { flex: 0.1, borderBottomWidth: 1 },
+    contentView: { flex: 0.9 },
+    bannerText: {
+        fontWeight: "700",
+        fontSize: 16,
+        textAlign: "center",
+        padding: 10
+    },
+    activityText: { color: "white" },
+    activityItemView: {
+        backgroundColor: "grey",
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 6,
+        justifyContent: "center",
+        marginHorizontal: 8,
+        minHeight: 32
+    },
+    dayView: {
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        justifyContent: "space-between",
+        height: 50,
+        alignItems: "center"
+    },
+    dayTextView: { alignItems: "center", justifyContent: "center" },
+    dayScheduleScrollView: { flex: 1, marginVertical: 10, flexDirection: "row" },
+    buttonText: { textAlign: "center" },
+    buttonView: {
+        padding: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    modalContainer: {
+        backgroundColor: "rgba(0,0,0,0.3)",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1
+    },
+    modalContent: {
+        backgroundColor: "white",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: 250,
+        position: "absolute",
+        bottom: 0
+    },
+    modalDaysContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: WIDTH
+    },
+    modalDaysButton: {
+        minHeight: 48,
+        maxHeight: 48,
+        minWidth: 48,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        justifyContent: "space-around",
+        borderRadius: 5,
+        backgroundColor: "#aaa",
+        flex: -1
+    },
+    modalDaysButtonSelected: {
+        backgroundColor: '#80ff00'
+    },
+    modalButtonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: WIDTH,
+        marginVertical: 15
+    },
+    dayButton: {
+        padding: 10,
+        justifyContent: "center",
+        borderRadius: 5,
+        alignItems: "center"
+    },
+    daySelectorView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: WIDTH,
+        paddingHorizontal: 10
+    }
 });
+
